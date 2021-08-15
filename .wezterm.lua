eb0f4c798c64a18b58a03eee4017fce7321ccd4f
koolay <koolay@163.com>
2021-08-15 12:51:08 +0800

    add wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require 'wezterm';
+return {
+  use_ime = true,
+  font_size = 16.0,
+  font_antialias = "Subpixel",
+  font_hinting = "Full",
+  font = wezterm.font("JetBrains Mono", {italic=false}),
+  color_scheme = "Batman",
+  keys = {
+    {key = "f", mods = "CTRL|SUPER", action = "ToggleFullScreen"},
+    {key = "=", mods = "CTRL", action = wezterm.action{SplitVertical = {domain = "CurrentPaneDomain"}}},
+    {key = "-", mods = "CTRL", action = wezterm.action{SplitHorizontal = {domain = "CurrentPaneDomain"}}},
+    {
+      key = "h",
+      mods = "CTRL",
+      action = wezterm.action {ActivatePaneDirection = "Left"}
+    }, {
+      key = ";",
+      mods = "CTRL",
+      action = wezterm.action {ActivatePaneDirection = "Right"}
+    },
+    {
+      key = "k",
+      mods = "CTRL",
+      action = wezterm.action {ActivatePaneDirection = "Up"}
+    }, {
+      key = "j",
+      mods = "CTRL",
+      action = wezterm.action {ActivatePaneDirection = "Down"}
+    }
+  }
+}
