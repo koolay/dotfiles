3609ec0180859515ca47529abbe7c877791a72e6
koolay <koolayhu@gmail.com>
2022-02-09 22:56:39 +0800

    Update themes

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,34 +1,36 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 return {
   use_ime = true,
   font_size = 16.0,
   font_antialias = "Subpixel",
   font_hinting = "Full",
   font = wezterm.font_with_fallback({
-      "JetBrainsMono Nerd Font",
+    "JetBrainsMono Nerd Font",
   }),
-  color_scheme = "Batman",
+  color_scheme = "Doom Peacock",
   keys = {
-    {key = "f", mods = "CTRL|SUPER", action = "ToggleFullScreen"},
-    {key = "=", mods = "CTRL", action = wezterm.action{SplitVertical = {domain = "CurrentPaneDomain"}}},
-    {key = "-", mods = "CTRL", action = wezterm.action{SplitHorizontal = {domain = "CurrentPaneDomain"}}},
+    { key = "f", mods = "CTRL|SUPER", action = "ToggleFullScreen" },
+    { key = "=", mods = "CTRL", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+    { key = "-", mods = "CTRL", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
     {
       key = "h",
       mods = "CTRL|SHIFT",
-      action = wezterm.action {ActivatePaneDirection = "Left"}
-    }, {
+      action = wezterm.action({ ActivatePaneDirection = "Left" }),
+    },
+    {
       key = "l",
       mods = "CTRL|SHIFT",
-      action = wezterm.action {ActivatePaneDirection = "Right"}
+      action = wezterm.action({ ActivatePaneDirection = "Right" }),
     },
     {
       key = "k",
       mods = "CTRL|SHIFT",
-      action = wezterm.action {ActivatePaneDirection = "Up"}
-    }, {
+      action = wezterm.action({ ActivatePaneDirection = "Up" }),
+    },
+    {
       key = "j",
       mods = "CTRL|SHIFT",
-      action = wezterm.action {ActivatePaneDirection = "Down"}
-    }
-  }
+      action = wezterm.action({ ActivatePaneDirection = "Down" }),
+    },
+  },
 }
--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here 1
 end
 
 
