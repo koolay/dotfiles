9ce01dabafdee82bebe11041fa33a5558ea820f1
koolay <koolayhu@gmail.com>
2023-09-13 01:56:22 +0800

    Update astronvim config

--- astronvim/user/plugins/community.lua
+++ astronvim/user/plugins/community.lua
@@ -4,6 +4,22 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+    import = "astrocommunity.colorscheme.everforest",
+    opts = {
+      everforest_background = "soft",
+    },
+  },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.editing-support.treesj" },
+  -- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  { import = "astrocommunity.bars-and-lines.scope-nvim" },
+  { import = "astrocommunity.project.project-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  { import = "astrocommunity.git.neogit" },
 }
--- astronvim/user/plugins/user.lua
+++ astronvim/user/plugins/user.lua
@@ -1,4 +1,25 @@
 return {
+  { "Bekaboo/dropbar.nvim" },
+  {
+    "onsails/lspkind.nvim",
+    opts = function(_, opts)
+      -- use codicons preset
+      opts.preset = "codicons"
+      -- set some missing symbol types
+      opts.symbol_map = {
+        Array = "",
+        Boolean = "",
+        Key = "",
+        Namespace = "",
+        Null = "",
+        Number = "",
+        Object = "",
+        Package = "",
+        String = "",
+      }
+      return opts
+    end,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
