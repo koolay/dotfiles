cc227891b194a44065d5b99a3092d0d6b572ba52
koolay <koolayhu@gmail.com>
2023-10-24 01:51:21 +0800

    Update nvim

--- astronvim/user/plugins/community.lua
+++ astronvim/user/plugins/community.lua
@@ -35,4 +35,8 @@ return {
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.vim-matchup" },
   { import = "astrocommunity.motion.grapple-nvim" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+
+  -- color/vim-highlighter
 }
